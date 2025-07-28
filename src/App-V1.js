6e7a1cd95b5cae299a4ce29a829c130f95c7b9f7
AbhishekWMG <ak89471@gmail.com>
2025-07-28 18:31:46 +0530

    comp-organize

--- src/App-V1.js
+++ src/App-V1.js
@@ -12,6 +12,7 @@ import ErrorMessage from "./ErrorMessage";
 import Loader from "./Loader";
 import StarRating from "./StarRating";
 import { ClipLoader } from "react-spinners";
+import MovieDetails from "./MovieDetails";
 
 
 
@@ -104,103 +105,103 @@ export default function App() {
 }
 
 
-function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
+// function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
 
-  const [userRating, setUserRating] = useState(0);
-  const [movie, setMovie] = useState({});
-  const [isLoading, setIsLoading] = useState(false);
+//   const [userRating, setUserRating] = useState(0);
+//   const [movie, setMovie] = useState({});
+//   const [isLoading, setIsLoading] = useState(false);
 
-  const isWatched = watched.map(movie => movie.imdbID).includes(selectedId);
-  const userWatchedRating = watched.find(movie => movie.imdbID === selectedId)?.userRating;
+//   const isWatched = watched.map(movie => movie.imdbID).includes(selectedId);
+//   const userWatchedRating = watched.find(movie => movie.imdbID === selectedId)?.userRating;
 
-  const { Title: title,
-    Year: year,
-    Poster: poster,
-    Runtime: runtime,
-    imdbRating,
-    Plot: plot,
-    Released: released,
-    Actors: actors,
-    Director: director,
-    Genre: genre } = movie;
+//   const { Title: title,
+//     Year: year,
+//     Poster: poster,
+//     Runtime: runtime,
+//     imdbRating,
+//     Plot: plot,
+//     Released: released,
+//     Actors: actors,
+//     Director: director,
+//     Genre: genre } = movie;
 
-  useEffect(function () {
-    async function getMovieDetails() {
-      setIsLoading(true);
-      const api_key = "4b4c6b8"
-      let fres = await fetch(`http://www.omdbapi.com/?apikey=${api_key}&i=${selectedId}`)
-      const data = await fres.json();
-      setMovie(data);
-      setIsLoading(false);
-    }
-    getMovieDetails();
-  }, [selectedId])
+//   useEffect(function () {
+//     async function getMovieDetails() {
+//       setIsLoading(true);
+//       const api_key = "4b4c6b8"
+//       let fres = await fetch(`http://www.omdbapi.com/?apikey=${api_key}&i=${selectedId}`)
+//       const data = await fres.json();
+//       setMovie(data);
+//       setIsLoading(false);
+//     }
+//     getMovieDetails();
+//   }, [selectedId])
 
-  function handleAdd() {
-    const newWatchedMovie = {
-      imdbID: selectedId,
-      title,
-      year,
-      poster,
-      imdbRating: Number(imdbRating),
-      runtime: Number(runtime.split(' ').at(0)),
-      userRating,
-    }
-    onAddWatched(newWatchedMovie);
-    onCloseMovie();
-  }
+//   function handleAdd() {
+//     const newWatchedMovie = {
+//       imdbID: selectedId,
+//       title,
+//       year,
+//       poster,
+//       imdbRating: Number(imdbRating),
+//       runtime: Number(runtime.split(' ').at(0)),
+//       userRating,
+//     }
+//     onAddWatched(newWatchedMovie);
+//     onCloseMovie();
+//   }
 
-  useEffect(function () {
-    document.title = `Movie | ${title} | ${year}`;
+//   useEffect(function () {
+//     document.title = `Movie | ${title} | ${year}`;
 
-    return function () {
-      document.title = 'usePopcorn'
-    }
-  }, [title])
+//     return function () {
+//       document.title = 'usePopcorn'
+//     }
+//   }, [title])
 
-  useEffect(function () {
+//   useEffect(function () {
 
-    function keydownCleanup(e) {
-      if (e.code === 'Escape') {
-        onCloseMovie();
-      }
-    }
+//     function keydownCleanup(e) {
+//       if (e.code === 'Escape') {
+//         onCloseMovie();
+//       }
+//     }
 
-    document.addEventListener('keydown', keydownCleanup)
+//     document.addEventListener('keydown', keydownCleanup)
 
-    return function () {
-      document.removeEventListener('keydown', keydownCleanup);
-    }
-  }, [onCloseMovie])
+//     return function () {
+//       document.removeEventListener('keydown', keydownCleanup);
+//     }
+//   }, [onCloseMovie])
 
-  return <div className="details">
-    {isLoading ? <ClipLoader color="green" /> :
-      <>
-        <header>
-          <button className="btn-back" onClick={onCloseMovie}>
-            &larr;
-          </button>
-          <img src={poster} alt={`Poster of ${movie}`} />
-          <div className="details-overview">
-            <h2>{title}</h2>
-            <p>{released} &bull; {runtime}</p>
-            <p>{genre}</p>
-            <p><span>🌟</span>{imdbRating} IMDB Rating</p>
-          </div>
-        </header>
-        <section>
-          <div className="rating">
-            {!isWatched ? <>
-              <StarRating maxRating={10} size={24} onSetRating={setUserRating} />
-              {userRating > 0 && <button className="btn-add" onClick={handleAdd}> + Add to list</button>}
-            </> : <p>You rated this movie {userWatchedRating} <span>✨</span></p>}
+//   return <div className="details">
+//     {isLoading ? <ClipLoader color="green" /> :
+//       <>
+//         <header>
+//           <button className="btn-back" onClick={onCloseMovie}>
+//             &larr;
+//           </button>
+//           <img src={poster} alt={`Poster of ${movie}`} />
+//           <div className="details-overview">
+//             <h2>{title}</h2>
+//             <p>{released} &bull; {runtime}</p>
+//             <p>{genre}</p>
+//             <p><span>🌟</span>{imdbRating} IMDB Rating</p>
+//           </div>
+//         </header>
+//         <section>
+//           <div className="rating">
+//             {!isWatched ? <>
+//               <StarRating maxRating={10} size={24} onSetRating={setUserRating} />
+//               {userRating > 0 && <button className="btn-add" onClick={handleAdd}> + Add to list</button>}
+//             </> : <p>You rated this movie {userWatchedRating} <span>✨</span></p>}
 
-          </div>
-          <p><em>{plot}</em></p>
-          <p>Starring {actors}</p>
-          <p>Directed By {director}</p>
-        </section></>
-    }
+//           </div>
+//           <p><em>{plot}</em></p>
+//           <p>Starring {actors}</p>
+//           <p>Directed By {director}</p>
+//         </section></>
+//     }
 
-  </div>
-}
+//   </div>
+// }
--- src/StarRating.js
+++ src/StarRating.js
@@ -1,4 +1,4 @@
-import { use, useState } from "react"
+import { useState } from "react"
 
 const containerStyle = {
     display: 'flex',
