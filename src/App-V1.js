63e6064825b98fb1eb794cf171e82b4977f42638
AbhishekWMG <ak89471@gmail.com>
2025-07-29 07:13:13 +0530

    feat: localStorage-movie-watchlist

--- src/App-V1.js
+++ src/App-V1.js
@@ -19,10 +19,13 @@ import MovieDetails from "./MovieDetails";
 export default function App() {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState(null);
+  const [watched, setWatched] = useState(function () {
+    const storedValue = localStorage.getItem('watched');
+    return JSON.parse(storedValue);
+  });
 
 
   function handleSelectMovie(id) {
@@ -40,6 +43,13 @@ export default function App() {
   function handleDeleteWatched(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id))
   }
+
+  //Adding watch list movies to localstorage
+  useEffect(function () {
+    localStorage.setItem("watched", JSON.stringify(watched))
+  }, [watched])
+
+
 
   //Fetching Movie
   useEffect(function () {
